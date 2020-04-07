acd0aa877dadff0cfc3b81f2c6d5f04e23dc2edf
alexsonablaza <61859191+alexsonablaza@users.noreply.github.com>
2020-04-07 16:56:26 -0700

    Update ServoMEGA.ino

--- ServoMEGA.ino
+++ ServoMEGA.ino
@@ -4,41 +4,56 @@
 int ServoP1 = 7; //Servo Pin
 Servo regServ1; //Servo Object 
 String inDat = " ";
-void setup() {
+
+void setup() 
+{
   // put your setup code here, to run once:
   regServ1.attach(ServoP1); //Attach 
   regServ1.write(0);
   Serial.begin (9600); //Activate Serial Comms.
   //ServoWriter(&regServ1);
 }
+
 int recv_byte = 0; //
 String inString = "";
+
 void loop()
 {
 ServoWriter(&regServ1);
-  
 brainValue = serial.readCSV();      //character string with latest brain values
 lgamma = serial.readLowGamma();     //reads low gamma waves
 hgamma = serial.readHighGamma();    //reads high gamma waves
 lbeta = serial.readLowBeta();       //reads low beta waves
 hbeta = serial.readHighBeta();      //reads high beta waves
-  
 }
+
 int ServoWriter(Servo *regServ1)
 {
       // put your main code here, to run repeatedly:
-  while (Serial.available()== 0) {
+  while (Serial.available()== 0) 
+  {
     //Check Serial for Data.
   }
-  inDat = Serial.readString();
+  
+  /*inDat = Serial.readString();        
   Serial.print("Number Entered: ");
   Serial.println(inDat);
   recv_byte =inDat.toInt(); 
-  if (isDigit(recv_byte) && (recv_byte >= 0) || (recv_byte <= 180)) {
+ */
+  
+  Serial.println(hgamma);
+  recv_byte = hgamma.toInt();
+  intLgamma = lgamma.toInt();
+  intLbeta = lbeta.toInt();
+  intHbeta = hbeta.toInt();
+  
+  if (isDigit(recv_byte) && (recv_byte >= 0) || (recv_byte <= 180)) 
+  {
   regServ1-> write(recv_byte);
   delayMicroseconds(100);
   }
-  else {
+  else 
+  {
       Serial.println ("Enter a Valid Number");
   }
 }
